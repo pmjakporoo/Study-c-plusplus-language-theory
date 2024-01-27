05816b80b9f5bd1946e755400007d542930946f6
Ryuisang <149366448+Ryusunok@users.noreply.github.com>
2024-01-27 15:53:15 +0900

    Create 14.UsingDcl2.cpp

---
+++ 01.열혈 c++  프로그래밍/02.예제코드/챕터1/14.UsingDcl2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main(void) {
+	int num = 20;
+	cout << "Hello world" << endl;
+	cout << "Hello " << "world!" << endl;
+	cout << num << ' ' << 'A';
+	cout << ' ' << 3.14 << endl;
+
+	return 0;
+}
