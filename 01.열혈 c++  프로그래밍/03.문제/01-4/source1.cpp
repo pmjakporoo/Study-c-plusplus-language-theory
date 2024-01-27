0e3f2eb0aae7caef64fa3de111e62abb1bd651d9
Ryuisang <149366448+Ryusunok@users.noreply.github.com>
2024-01-27 15:33:15 +0900

    Create source1.cpp

---
+++ 01.열혈 c++  프로그래밍/03.문제/01-4/source1.cpp
@@ -0,0 +1,10 @@
+#include "header1.h"
+#include <iostream>
+
+void BestComImp1::SimpleFunc() {
+	std::cout << "BestCom이 정의한 함수" << std::endl;
+}
+
+void ProgComImp1::SimpleFunc() {
+	std::cout << "ProgCom이 정의한 함수" << std::endl;
+}
