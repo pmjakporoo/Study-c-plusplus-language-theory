498c2b74019d14a04714a9fb4f7b6810a3ab436a
Ryuisang <149366448+Ryusunok@users.noreply.github.com>
2024-02-01 16:05:49 +0900

    Create Circle.h

---
+++ 01.열혈 c++  프로그래밍/03.문제/04-2/Circle.h
@@ -0,0 +1,21 @@
+#ifndef CIRCLE_HEADER
+#define CIRCLE_HEADER
+
+#include "Point.h"
+
+class Circle {
+	Point center;
+	int rad;
+
+public:
+	void Init(int x1, int y1, int rad1) {
+		center.Init(x1, y1);
+		rad = rad1;
+	}
+
+	void ShowCircleInfo() const {
+		center.ShowPointInfo();
+		cout << "반지름 : " << rad << endl;
+	}
+};
+#endif
