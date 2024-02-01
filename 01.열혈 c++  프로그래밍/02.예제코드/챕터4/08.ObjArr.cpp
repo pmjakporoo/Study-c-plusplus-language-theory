61f9f9d7263a91afb50dbd1875a9f72b3e931c2d
Ryuisang <149366448+Ryusunok@users.noreply.github.com>
2024-02-01 22:27:00 +0900

    Create 08.ObjArr.cpp

---
+++ 01.열혈 c++  프로그래밍/02.예제코드/챕터4/08.ObjArr.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+class Person {
+	char* name;
+	int age;
+public:
+	//Person(char* myname, int myage):age(myage) {
+	//	name = new char[strlen(myname) + 1];
+	//	strcpy(name, myname);
+	//}
+
+	Person():age(0) {
+		name = NULL;
+		cout << "Called person()" << endl;
+	}
+
+	void SetPersonInfo(char* myname, const int myage) {
+		name = myname;
+		age = myage;
+	}
+
+	void ShowPersonInfo() const {
+		cout << "이름: " << name << endl;
+		cout << "나이: " << age << endl;
+	}
+
+	~Person() {
+		delete []name;
+		cout << "called destructor!" << endl;
+	}
+};
+
+int main(void) {
+	Person parr[3];
+	char namestr[100];
+	char* strptr;
+	int age;
+	int len;
+
+	for (int i = 0; i < 3; i++) {
+		cout << "이름: ";
+		cin >> namestr;
+		cout << "나이: ";
+		cin >> age;
+		len = strlen(namestr) + 1;
+		strptr = new char[len];
+		strcpy(strptr, namestr);
+		parr[i].SetPersonInfo(strptr, age);
+	}
+
+	parr[0].ShowPersonInfo();
+	parr[1].ShowPersonInfo();
+	parr[2].ShowPersonInfo();
+
+	return 0;
+}
