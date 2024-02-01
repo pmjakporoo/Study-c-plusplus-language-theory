cd2d3139457a9f4c37100654b3e90cc40743dcd2
Ryuisang <149366448+Ryusunok@users.noreply.github.com>
2024-02-01 16:05:38 +0900

    Create Point.h

---
+++ 01.열혈 c++  프로그래밍/03.문제/04-2/Point.h
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+#ifndef POINT_HEADER
+#define POINT_HEADER
+
+class Point {
+	int xpos, ypos;
+
+public:
+	void Init(int x, int y) {
+		xpos = x,
+		ypos = y;
+	}
+
+	void ShowPointInfo() const {
+		cout << "[" << xpos << ", " << ypos << "]" << endl;
+	}
+};
+#endif
