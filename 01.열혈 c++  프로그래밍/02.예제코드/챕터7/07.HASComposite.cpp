76ec19c63892ec06c07744a7ecee0ae6353386be
Ryuisang <149366448+Ryusunok@users.noreply.github.com>
2024-02-04 16:40:50 +0900

    Create 07.HASComposite.cpp

---
+++ 01.열혈 c++  프로그래밍/02.예제코드/챕터7/07.HASComposite.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+class Gun {
+	int bullet;
+public:
+	Gun(int bnum) : bullet(bnum) {}
+	void Shot() {
+		cout << "BBANG!" << endl;
+		bullet--;
+	}
+};
+
+class Police {
+	int handcuffs;
+	Gun* pistol;
+
+public:
+	Police(int bnum, int bcuff) : handcuffs(bcuff) {
+		if (bnum > 0) {
+			pistol = new Gun(bnum);
+		}
+		else {
+			pistol = NULL;
+		}
+	}
+	void PutHandcuff() {
+		cout << "SNAP!" << endl;
+		handcuffs--;
+	}
+
+	void Shot() {
+		if (pistol == NULL) {
+			cout << "Hut BBANG!" << endl;
+		}
+		else {
+			pistol->Shot();
+		}
+	}
+
+	~Police() {
+		if (pistol != NULL) {
+			delete pistol;
+		}
+	}
+};
+
+int main(void) {
+	Police pman1(5, 3);
+	pman1.Shot();
+	pman1.PutHandcuff();
+
+	Police pman2(0, 3);
+	pman2.Shot();
+	pman2.PutHandcuff();
+	return 0;
+}
