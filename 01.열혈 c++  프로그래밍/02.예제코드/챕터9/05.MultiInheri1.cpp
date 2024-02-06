e5edd77e7130304426bfbf05b528ef1158e315ef
Ryuisang <149366448+Ryusunok@users.noreply.github.com>
2024-02-06 16:23:19 +0900

    Create 05.MultiInheri1.cpp

---
+++ 01.열혈 c++  프로그래밍/02.예제코드/챕터9/05.MultiInheri1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class BaseOne {
+public:
+	void SimpleFunc() {
+		cout << "BaseOne" << endl;
+	}
+};
+
+class BaseTwo {
+public:
+	void SimpleFunc() {
+		cout << "BaseTwo" << endl;
+	}
+};
+
+class MultiDerived : public BaseOne, BaseTwo {
+public:
+	void ComplexFunc() {
+		BaseOne::SimpleFunc();
+		BaseTwo::SimpleFunc();
+	}
+};
+
+int main(void) {
+	MultiDerived mdr;
+	mdr.ComplexFunc();
+}
